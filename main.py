a13a9adc109db4dc664790218c9aaa2f21015b5c
Winni_Lina <9811011sa01@gmail.com>
2021-05-21 02:46:54 +0900

    5-21

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
 import discord
-import asyncio
- 
-client = discord.Client()
- 
+from discord.ext import commands
+import os
+
+client = commands.Bot(command_prefix = '-')
+
 @client.event
-async def on_ready(): 
-    print("디스코드 봇 로그인이 완료되었습니다.")
-    print("디스코드봇 이름:" + client.user.name)
-    print("디스코드봇 ID:" + str(client.user.id))
-    print("디스코드봇 버전:" + str(discord.__version__))
-    print('------')
-    await client.change_presence(status=discord.Status.online, activity=discord.Game("서술"))
- 
-@client.event
-async def on_message(message): 
-    content = message.content 
-    guild = message.guild 
-    author = message.author 
-    channel = message.channel 
-    if content.startswith("!test"): 
-        await message.channel.send("test" + message.content) 
-    if content == "!ping": 
-        await message.channel.send("Pong!") 
- 
-client.run('ODQ0OTgxMzI2MjE4MjY0NjA3.YKaULw.pc0Gb0QGYfp_4106lNBnlx-c_UU')
+async def on_ready():
+
+  # [discord.Status.online = 온라인],[discord.Status.idle = 자리비움],[discord.Status.dnd = 다른용무],[discord.Status.offline = 오프라인]
+  await client.change_presence(status=discord.Status.online)
+
+  await client.change_presence(activity=discord.Game(name="게임 하는중"))
+  #await client.change_presence(activity=discord.Streaming(name="스트림 방송중", url='링크'))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.listening, name="노래 듣는중"))
+  #await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="영상 시청중"))
+  
+  print("봇 이름:",client.user.name,"봇 아이디:",client.user.id,"봇 버전:",discord.__version__)
+
+
+client.run(os.environ['token'])
