76eafcc88db5827c3cacd3efeab65b868c87501f
Winni_Lina <9811011sa01@gmail.com>
2021-05-21 13:56:33 +0900

    .

---
+++ commands/help.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+import time
+
+class Help(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    #commands
+    @commands.command()
+    async def 도움(self, ctx):
+        await ctx.trigger_typing()
+        embed = discord.Embed(title="아직 제작중입니다.")
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(Help(client))
