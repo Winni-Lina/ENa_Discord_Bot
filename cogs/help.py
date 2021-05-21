d1c5461a1f03b31566899ff4367b0185b8b3fdd0
Winni_Lina <9811011sa01@gmail.com>
2021-05-21 14:43:32 +0900

    5-21 2:43

--- cogs/help.py
+++ cogs/help.py
@@ -2,17 +2,16 @@ import discord
 from discord.ext import commands
 import time
 
-class Help(commands.Cog):
+class Test(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     #cogs
     @commands.command()
-    async def 도움(self, ctx):
-        await ctx.trigger_typing()
-        embed = discord.Embed(title="아직 제작중입니다.")
-        await ctx.send(embed=embed)
+    async def 테스트(self, ctx):
+        # 명령어 내용 입력
+        await ctx.send('OK')
 
 
 def setup(client):
-    client.add_cog(Help(client))
+    client.add_cog(Test(client))
