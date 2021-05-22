9263a1d7d3d141a4d75609a974496a35f7cb8151
YuJin Kim <55020976+Winni-Lina@users.noreply.github.com>
2021-05-22 20:16:56 +0900

    5-22

--- cogs/help.py
+++ cogs/help.py
@@ -1,18 +1,19 @@
 import discord
 from discord.ext import commands
-import time
 
 
-class Test(commands.Cog):
+class Help(commands.Cog):
     def __init__(self, client):
         self.client = client
 
     # cogs
     @commands.command()
-    async def 테스트(self, ctx):
+    async def 도움(self, ctx):
         # 명령어 내용 입력
-        await ctx.send('OK')
+        await ctx.trigger_typing()
+        embed = discord.Embed(title="아직 제작중입니다.")
+        await ctx.send(embed=embed)
 
 
 def setup(client):
-    client.add_cog(Test(client))
+    client.add_cog(Help(client))
--- main.py
+++ main.py
@@ -6,8 +6,7 @@ import re
 
 client = commands.Bot(command_prefix='#', help_command=None)
 
-status = cycle(['미완성 봇'])  # 번갈아 표시할 상메
-
+status = cycle(['미완성 봇'])   # 번갈아 표시할 상메
 
 @client.event
 async def on_ready():
@@ -17,10 +16,8 @@ async def on_ready():
 
 @tasks.loop(seconds=10)
 async def change_status():
-    await client.change_presence(status=discord.Status.online, activity=discord.Game(next(status)))
-
-
-for filename in os.listdir('./cogs'):  # cogs 폴더에 있는 명령어를 하나 씩 읽어옴
+    await client.change_presence(status=discord.Status.online,activity=discord.Game(next(status)))
+for filename in os.listdir('./cogs'):   # cogs 폴더에 있는 명령어를 하나 씩 읽어옴
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
