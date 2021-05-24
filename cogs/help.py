aedad37cc268f879e495201a2a4629b00e87a110
Winni_Lina <9811011sa01@gmail.com>
2021-05-24 09:53:34 +0900

    5/24

--- cogs/help.py
+++ cogs/help.py
@@ -9,7 +9,13 @@ class Help(commands.Cog):
     @commands.command()
     async def 도움(self, ctx):
         await ctx.trigger_typing()
-        embed = discord.Embed(title="아직 제작중입니다.")
+
+        embed = discord.Embed(title="도움말", description="이 봇은 아직 미완성인 봇입니다.", color=0x62c1cc)
+        embed.add_field(name='**도움**', value='명령어 목록을 보여줍니다.', inline=False)
+        embed.add_field(name='**명령어**', value='`핑`, `초대링크`')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/843370400225820672/844955931531935804/ReactNative-snapshot-image4472701035798000406.jpg')
+        embed.set_footer(text='made by 루크#8209, Jin。#5499', icon_url='https://cdn.discordapp.com/attachments/843370400225820672/844955931531935804/ReactNative-snapshot-image4472701035798000406.jpg')
+        embed.add_field(name='명령어 추가 요청', value='명령어 형태로 제작할 예정입니다.', inline=False)
         await ctx.send(embed=embed)
 
 
---
+++ cogs/inviteLink.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class InviteLink(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    # cogs
+    @commands.command()
+    async def 초대링크(self, ctx):
+        await ctx.trigger_typing()
+
+        embed = discord.Embed(title='Disbot 봇을 초대할 수 있는 링크입니다', color=0x62c1cc)
+        embed.add_field(name='초대링크', value='https://discord.com/api/oauth2/authorize?client_id=844981326218264607&permissions=0&scope=bot')
+        await ctx.send(embed=embed)
+
+
+def setup(client):
+    client.add_cog(InviteLink(client))
