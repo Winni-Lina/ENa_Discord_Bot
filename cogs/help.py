c489602e34ef02f6606b930b144c0a257381275a
Winni_Lina <9811011sa01@gmail.com>
2021-09-27 12:23:26 +0900

    Edit help.py, inviteLink.py

--- cogs/help.py
+++ cogs/help.py
@@ -13,8 +13,8 @@ class Help(commands.Cog):
         embed = discord.Embed(title="도움말", description="이 봇은 아직 미완성인 E.Na봇입니다.", color=0x62c1cc)
         embed.add_field(name='**도움**', value='명령어 목록을 보여줍니다.', inline=False)
         embed.add_field(name='**명령어**', value='`핑`, `초대링크`, `영화랭킹`')
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/843370400225820672/844955931531935804/ReactNative-snapshot-image4472701035798000406.jpg')
-        embed.set_footer(text='made by 루크#8209, Jin。#5499', icon_url='https://cdn.discordapp.com/attachments/843370400225820672/844955931531935804/ReactNative-snapshot-image4472701035798000406.jpg')
+        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/891875793783902218/891875843826135040/29841_R8odfZL3.png')
+        embed.set_footer(text='made by BLUE#7608', icon_url='https://cdn.discordapp.com/avatars/777496131177414656/eedb299fd8b1860a80b15a5c0c5d5168.webp?size=1024')
         embed.add_field(name='명령어 추가 요청', value='명령어 형태로 제작할 예정입니다.', inline=False)
         await ctx.send(embed=embed)
 
--- main.py
+++ main.py
@@ -2,22 +2,22 @@ import discord
 from discord.ext import commands, tasks
 from itertools import cycle
 import os
-import re
 
-token = open("token", "r").readline()
-client = commands.Bot(command_prefix='~e', help_command=None)
+token = open("./ignore/token.txt", "r").readline()
+client = commands.Bot(command_prefix='~e ', help_command=None)
 
-status = cycle(['E.Na 봇 작동중', '테스트 중입니다.'])
+status = cycle(['E.Na 봇 작동', '테스트'])
 
 @client.event
 async def on_ready():
     change_status.start()
+    print("이나 봇 작동 시작합니다.")
 
 @tasks.loop(seconds=10)
 async def change_status():
     await client.change_presence(status=discord.Status.online, activity=discord.Game(next(status)))
 
-for filename in os.listdir('./Cogs'):
+for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f'cogs.{filename[:-3]}')
 
